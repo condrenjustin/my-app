1a76a756eae82b92f0b25ddc6f74cd45be1a22de
Justin Condren <justin.l.condren@vanderbilt.edu>
2020-05-17 16:30:17 -0400

    Added gray box that displays info of chosen row

--- src/actions/homeActions.ts
+++ src/actions/homeActions.ts
@@ -2,6 +2,7 @@ export enum HomeActions {
 		SET_HOME_API_FLAG = 'SET_HOME_API_FLAG',
 		SET_HOME_API_DATA = 'SET_HOME_API_DATA',
 		SET_TABLE = 'SET_TABLE',
+		SAVE_DATA_INDEX = 'SAVE_DATA_INDEX'
 	}
 
 export const setHomeFlagActionCreator = (flag:string, value:boolean) => {
@@ -26,5 +27,12 @@ export const setTableActionCreator = (data:string[][], columns:string[]) => {
 	return {
 		type: HomeActions.SET_TABLE,
 		payload: { data, columns },
+	};
+};
+
+export const saveDataIndexActionCreator = (selectedDataIndex:number) => {
+	return {
+		type: HomeActions.SAVE_DATA_INDEX,
+		payload: { selectedDataIndex },
 	};
 };
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Table from '../common/table';
 import styles from './home.module.scss';
+import { Paper, Grid, Typography } from '@material-ui/core'
 
 export interface HomeProps{
   tableHeight:number;
@@ -9,6 +10,7 @@ export interface HomeProps{
   tableTitle:string;
   data:any[];
   columns:any[];
+  selectedDataIndex:any;
 }
 
 const Home = (props:HomeProps) => {
@@ -19,6 +21,7 @@ const Home = (props:HomeProps) => {
     tableTitle,
     data,
     columns,
+    selectedDataIndex,
   } = props;
 
   return (
@@ -31,6 +34,42 @@ const Home = (props:HomeProps) => {
         tableTitle={tableTitle}
         tableStyle={tableStyle}
       />
+      <Paper style={{backgroundColor: '#E5E5E5'}} className={styles.paperStyles} variant="outlined">
+        <Grid container justify="space-around">
+          <Grid item>
+            <Typography align="center" variant="h4">
+              Name
+            </Typography>
+            <Typography align="center">
+              {selectedDataIndex.name}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography align="center" variant="h4">
+              Company
+            </Typography>
+            <Typography align="center">
+              {selectedDataIndex.company}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography align="center" variant="h4">
+              City
+            </Typography>
+            <Typography align="center">
+            {selectedDataIndex.city}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography align="center" variant="h4">
+              State
+            </Typography>
+            <Typography align="center">
+              {selectedDataIndex.state}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 };
